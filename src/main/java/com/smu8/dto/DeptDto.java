425f2be17f33f1cecac6d2662a46238c3d49241e
develckm86 <develckm86@gmail.com>
2026-04-07 11:20:42 +0900

    emp,dept dto 생성

---
+++ src/main/java/com/smu8/dto/DeptDto.java
@@ -0,0 +1,39 @@
+package com.smu8.dto;
+
+public class DeptDto {
+    private int deptno; //pk Number(2)
+    private String dename;// Varchar2(14)
+    private String loc;// Varchar2(13)
+    @Override
+    public String toString() {
+        return "{" +
+                "deptno=" + deptno +
+                ", dename='" + dename + '\'' +
+                ", loc='" + loc + '\'' +
+                '}'+'\n';
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(int deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDename() {
+        return dename;
+    }
+
+    public void setDename(String dename) {
+        this.dename = dename;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+}
